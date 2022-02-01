83f9b5872d0247cb2fa6862ce472f2c711bb6c48
Niall <50669114+niallVR@users.noreply.github.com>
2022-02-01 11:47:53 +0000

    Flurl: Migrated project

---
+++ Senko.Flurl/Extensions/FlurlHttpSettingsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Flurl.Http.Configuration;
+using NiallVR.Senko.Flurl.Entities;
+
+namespace NiallVR.Senko.Flurl.Extensions;
+
+public static class FlurlHttpSettingsExtensions {
+    private static readonly SystemJsonSerializer Serializer = new();
+
+    /// <summary>
+    /// Changes the settings to use System.Json over Flurl's default provider.
+    /// </summary>
+    /// <param name="settings">The settings to edit.</param>
+    /// <returns>The edited settings.</returns>
+    public static FlurlHttpSettings UsingSystemJson(this FlurlHttpSettings settings) {
+        settings.JsonSerializer = Serializer;
+        return settings;
+    }
+
+    /// <summary>
+    /// Changes the settings to use System.Json over Flurl's default provider.
+    /// </summary>
+    /// <param name="settings">The settings to edit.</param>
+    /// <param name="options">The serializer options to use.</param>
+    /// <returns>The edited settings.</returns>
+    public static FlurlHttpSettings UsingSystemJson(this FlurlHttpSettings settings, JsonSerializerOptions options) {
+        settings.JsonSerializer = new SystemJsonSerializer(options);
+        return settings;
+    }
+
+    /// <summary>
+    /// Changes the default behaviour to disable exception throwing when a server returns a non 200 code.
+    /// </summary>
+    /// <param name="settings">The settings to edit.</param>
+    /// <returns>The edited settings.</returns>
+    public static FlurlHttpSettings DontThrowOnError(this FlurlHttpSettings settings) {
+        settings.AllowedHttpStatusRange = "*";
+        return settings;
+    }
+}
---
+++ Senko.Flurl/Extensions/FlurlRequestExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Flurl.Http;
+
+namespace NiallVR.Senko.Flurl.Extensions;
+
+public static class FlurlRequestExtensions {
+    /// <summary>
+    /// Changes the settings to use System.Json over Flurl's default provider.
+    /// </summary>
+    /// <param name="request">The request to edit.</param>
+    /// <returns>The edited request.</returns>
+    public static IFlurlRequest UsingSystemJson(this IFlurlRequest request) {
+        request.Settings.UsingSystemJson();
+        return request;
+    }
+
+    /// <summary>
+    /// Changes the settings to use System.Json over Flurl's default provider.
+    /// </summary>
+    /// <param name="request">The request to edit.</param>
+    /// <param name="options">The serializer options to use.</param>
+    /// <returns>The edited request.</returns>
+    public static IFlurlRequest UsingSystemJson(this IFlurlRequest request, JsonSerializerOptions options) {
+        request.Settings.UsingSystemJson(options);
+        return request;
+    }
+
+    /// <summary>
+    /// Changes the default behaviour to disable exception throwing when a server returns a non 200 code.
+    /// </summary>
+    /// <param name="request">The request to edit.</param>
+    /// <returns>The edited request.</returns>
+    public static IFlurlRequest DontThrowOnError(this IFlurlRequest request) {
+        request.Settings.DontThrowOnError();
+        return request;
+    }
+}
