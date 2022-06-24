b82a61b298ee6a2080095284eda68bfdf4506c62
Niall <50669114+niallVR@users.noreply.github.com>
2022-06-24 08:12:21 +0100

    Project: Remove the Bootstrap and Discord projects
    The plan is to move these to their own repository, and to make Senko just a util library.

--- Examples/Senko.Discord.Example/Config/DiscordConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace Senko.Discord.Example.Config; 
-
-public class DiscordConfig {
-    /// <summary>
-    /// The token for the Discord bot user.
-    /// </summary>
-    public string Token { get; init; }
-}
